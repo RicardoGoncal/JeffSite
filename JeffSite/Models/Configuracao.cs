54ec7838af78a7f9a97597c2f31550424e0cf44b
FabioVeiga <droidbinho@gmail.com>
2020-12-27 11:38:32 -0300

    Ajuste configvuracao

--- JeffSite/Models/Configuracao.cs
+++ JeffSite/Models/Configuracao.cs
@@ -9,12 +9,12 @@ namespace JeffSite.Models
         
         [Required (ErrorMessage = "Por favor, inserir {0}!")]
         [Display(Name = "E-mail para contato")]
+        [EmailAddress (ErrorMessage = "Necessário email válido!")]
         public string ContactEmail { get; set; }
-        //[StringLength(30, MinimumLength = 3, ErrorMessage = "{0} requer entre {2} e {1} caracteres!")]
         [Required (ErrorMessage = "Por favor, inserir {0}!")]
         [Display(Name = "Foto logotipo")]
         public string ImgProfile { get; set; }
-        
+
         [Required (ErrorMessage = "Por favor, inserir {0}!")]
         [Display(Name = "Imagem do perfil")]
         public string ImgLogo { get; set; }
